b6b364a7f44e42bf1f24dda0e84078e74cfbac8c
shangxian Piao <sj212131@hotmail.com>
2021-03-28 14:41:20 -0400

    fixed license function

---
+++ demo/README.md
@@ -0,0 +1,31 @@
+# 3333333333333333
+  (https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+  # Description
+  4444444444444444
+
+  # Table of Contents 
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [Questions](#questions)
+   
+  # Installation
+  The following necessary dependencies must be installed to run the application properly: 5555555555555555
+
+  # Usage
+  ​This application is used for 66666666666666666.
+
+  # License
+  This project is license under the MIT license.
+
+  # Contributing
+  ​Contributors: 77777777777777777
+
+  # Tests
+  To run tests, you need to run the following command: 88888888888888.
+
+  # Questions
+  If you have any questions about the repo https://github.com/111111111111111 , open an issue or contact directly 22222222222222222.
+  
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,31 +2,32 @@
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
     if (license === 'MIT'){
-      const licenseBadge = 'MIT-yellow';
+      var licenseBadge = 'MIT-yellow';
     } else if (license === 'GPL') {
-      const licenseBadge = 'GPLv3-blue';
+      var licenseBadge = 'GPLv3-blue';
     } else if (license === 'Apache') {
-      const licenseBadge = 'Apache%202.0-blue';
+      var licenseBadge = 'Apache%202.0-blue';
     } else {
-      const licenseBadge = '';
+      var licenseBadge = '';
     }
-    renderLicenseLink(license, licenseBadge) 
+    return renderLicenseLink(license, licenseBadge) 
   }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license, licenseBadge) {
     if (license === ''){
-      const licenseLink = "" ; 
+      var licenseLink = "" ; 
   } else {
-      const licenseLink = `\r(https://img.shields.io/badge/License-${licenseBadge}.svg)](https://opensource.org/licenses/${license})`;
+      var licenseLink = `\r(https://img.shields.io/badge/License-${licenseBadge}.svg)](https://opensource.org/licenses/${license})`;
   }
+  return licenseLink
 }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-    renderLicenseBadge(license) 
+  return renderLicenseBadge(license) 
 };
 
 
